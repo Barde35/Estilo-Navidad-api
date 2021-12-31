6ff044e44c61685010bb7c05e6d71d91584d2f66
Agustin Bardelli <bardelli35@gmail.com>
2021-12-31 13:03:44 -0300

    heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbUser = process.env.dbUser
 const dbPass = process.env.dbPass
 
-const url = `mongodb+srv://Barde35:Piojos01.@cluster0.jhegx.mongodb.net/estilonavidad?retryWrites=true&w=majority`
+const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.jhegx.mongodb.net/estilonavidad?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
     useNewUrlParser: true,
